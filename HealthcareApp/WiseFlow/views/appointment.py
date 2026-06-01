07da66da0950d325ff585630c4d483a455afe6dd
Manasa Kalipu <manasa.kalipu@vassardigital.ai>
2026-06-01 11:16:43 +0530

    Completed upto Chatbot

--- HealthcareApp/WiseFlow/views/appointment.py
+++ HealthcareApp/WiseFlow/views/appointment.py
@@ -4,15 +4,32 @@ from rest_framework.response import Response
 from appointments.models import Appointment
 from WiseFlow.serializers import AppointmentSerializer
 
+
 class AppointmentView(APIView):
+
     def get(self, request):
         appointments = Appointment.objects.all()
         serializer = AppointmentSerializer(appointments, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+
+        existing = Appointment.objects.filter(
+            doctor=request.data["doctor"],
+            appointment_date=request.data["appointment_date"],
+            appointment_time=request.data["appointment_time"]
+        ).exists()
+
+        if existing:
+            return Response(
+                {"error": "Doctor already has an appointment at this time slot"},
+                status=400
+            )
+
         serializer = AppointmentSerializer(data=request.data)
+
         if serializer.is_valid():
-            appointment = serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            serializer.save()
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)
